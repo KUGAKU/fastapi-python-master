281445743654603d291dd57eda90bc1320f7dbf7
KUGAKU <githubgemini.0521@gmail.com>
2023-08-30 08:35:26 +0900

    add langchain

--- business_logic/chat_service.py
+++ business_logic/chat_service.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Any, Generator, Optional
+from typing import Any, Generator
 from injector import inject
 from data_access.chat_repository import AbstractChatRepository
+from data_source.langchain.langchain_chat_model_factory import LangchainChatModelFactory
 from data_source.openai_data_source import AbstractOpenaiDataSource
 from data_transfer_object.chat_completion_chunk import ChatCompletionChunk
 from models.message_type import MessageTypeEnum
 from utils.message_buffer import MessageBufferManager
+from langchain.schema import HumanMessage
 from utils.server_sent_event_maker import (
     ChatSSEData,
     ChatSSEEvent,
@@ -52,21 +54,40 @@ class ChatService(AbstractChatService):
         try:
             messageBufferManager = MessageBufferManager()
 
-            response = self.openai_data_source.get_chat_stream_chunk_content(
-                chat_message
-            )
-            for event in response:
-                event_content = self.extract_event_content(event)
-                if event_content:
-                    messageBufferManager.add_to_buffer(event_content)
-                    chatSSEData = ChatSSEData(
-                        chat_content=event_content, conversation_id=None
-                    )
-                    yield ServerSentEventMaker.create_sse_packet(
-                        ChatSSEEvent.PROGRESSION, chatSSEData
-                    )
+            def handle_token(token: str):
+                messageBufferManager.add_to_buffer(token)
 
+            factory = LangchainChatModelFactory()
+            azure_chat_model = factory.create_instance(handle_token)
+            azure_chat_model(
+                [
+                    HumanMessage(
+                        content=chat_message,
+                    )
+                ]
+            )
+            for token in messageBufferManager.get_buffer():
+                chatSSEData = ChatSSEData(chat_content=token, conversation_id=None)
+                yield ServerSentEventMaker.create_sse_packet(
+                    ChatSSEEvent.PROGRESSION, chatSSEData
+                )
             message = messageBufferManager.get_joined_buffer()
+
+            # response = self.openai_data_source.get_chat_stream_chunk_content(
+            #     chat_message
+            # )
+            # for event in response:
+            #     event_content = self.extract_event_content(event)
+            #     if event_content:
+            #         messageBufferManager.add_to_buffer(event_content)
+            #         chatSSEData = ChatSSEData(
+            #             chat_content=event_content, conversation_id=None
+            #         )
+            #         yield ServerSentEventMaker.create_sse_packet(
+            #             ChatSSEEvent.PROGRESSION, chatSSEData
+            #         )
+
+            # message = messageBufferManager.get_joined_buffer()
 
             # 既存の会話か新しい会話かによってIDが変わる為、最終的に選択されるID
             resolved_conversation_id = ""
