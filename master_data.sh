155dc09fcb640ec8e63de0315999386ebb1fcb30
KUGAKU <githubgemini.0521@gmail.com>
2023-09-14 01:10:56 +0900

    master_data.sh 修正

--- master_data.sh
+++ master_data.sh
@@ -7,17 +7,23 @@ from models.message_type import MessageType, MessageTypeEnum
 from settings import Session
 session = Session()
 
-message_type = MessageType()
-message_type.message_type_id = MessageTypeEnum.HUMAN.value
-message_type.message_type_name = 'Human'
-session.add(message_type)
+existing_message_type = session.query(MessageType).filter_by(message_type_name='Human').first()
+print(f'existing_message_type: message_type_id:{existing_message_type.message_type_id}, message_type_name:{existing_message_type.message_type_name}')
+if not existing_message_type:
+    message_type = MessageType()
+    message_type.message_type_id = MessageTypeEnum.HUMAN.value
+    message_type.message_type_name = 'Human'
+    session.add(message_type)
+    session.commit()
 
-message_type = MessageType()
-message_type.message_type_id = MessageTypeEnum.ARTIFICIAL_INTELLIGENCE.value
-message_type.message_type_name = 'Artificial Intelligence'
-session.add(message_type)
-
-session.commit()
+existing_message_type = session.query(MessageType).filter_by(message_type_name='Artificial Intelligence').first()
+print(f'existing_message_type: message_type_id:{existing_message_type.message_type_id}, message_type_name:{existing_message_type.message_type_name}')
+if not existing_message_type:
+    message_type = MessageType()
+    message_type.message_type_id = MessageTypeEnum.ARTIFICIAL_INTELLIGENCE.value
+    message_type.message_type_name = 'Artificial Intelligence'
+    session.add(message_type)
+    session.commit()
 
 print('success!')
 "
