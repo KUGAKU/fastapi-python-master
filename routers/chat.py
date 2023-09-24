4bcdb511204d4403f25bd15fc34ad0cfaf2f7ddd
KUGAKU <githubgemini.0521@gmail.com>
2023-09-24 17:26:33 +0900

    add authorization in swagger ui

--- routers/chat.py
+++ routers/chat.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from injector import Injector
 from di_modules.chat_di import ChatDi
 
@@ -9,9 +10,11 @@ from schemas.chat import ChatRequest
 
 router = APIRouter()
 
+security = HTTPBearer()
+
 
 @router.post("/")
-def chat(request: ChatRequest):
+def chat(request: ChatRequest, _: HTTPAuthorizationCredentials = Security(security)):
     injector = Injector([ChatDi()])
     chat_controller = injector.get(ChatController)
     return chat_controller.startChat(request.chat_message, request.conversation_id)
